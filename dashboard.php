69f65dc28a8bfb4185a8f20d47b38c00a2716449
nabang97 <parawitayolanda@gmail.com>
2017-07-05 20:42:15 +0700

    isi dashboard
    kalau ada tambahan.. tambahin aja ya :v:

--- dashboard.php
+++ dashboard.php
@@ -18,11 +18,53 @@
 	</div>
 <!-- banner -->
 
-<!-- dashboard -->
+<!-- Menu -->
 <br>
 <?php include 'view/menu.php'; ?>
-<!-- //dashboard -->
-
+<!-- end Menu -->
+<!-- Dashboard -->
+<br>
+<div class="container">
+<form>
+<div class="form-group">
+		  <label for="posisi">Title</label>
+		  <input type="text" class="form-control" name="posisi" placeholder="">
+	</div>
+	<div class="form-group">
+		  <label for="sel1">Job Type</label>
+		  <select class="form-control" id="sel1">
+		    <option value="">Permanent</option>
+		    <option value="">Contract</option>
+		  </select>
+	</div>
+	<div class="form-group">
+		  <label for="posisi">Position</label>
+		  <input type="text" class="form-control" name="posisi" placeholder="">
+	</div>
+	<div class="form-group">
+		  <label for="posisi">Salary</label>
+		  <input type="text" class="form-control" name="posisi" placeholder="">
+	</div>
+	<div class="form-group">
+		  <label for="posisi">Requirements</label>
+		  <textarea class="form-control" rows="5" id="comment"></textarea>
+	</div>
+	<div class="form-group">
+		  <label for="posisi">Location</label>
+		  <select class="form-control" id="sel2">
+		    <option value="">Padang</option>
+		    <option value="">Payakumbuh</option>
+		  </select>
+	</div>
+	<div class="form-group">
+		    <label for="exampleInputFile">Upload Pamflet / Brosur (optional)</label><br><br>
+		    <input type="file" id="pamflet">
+		    <p class="help-block">nb : ekstensi yang diterima hanya berupa jpg/ jpeg/ png</p>
+	</div>
+	<center><button type="submit" class="btn btn-default">Submit</button></center><br>
+</form>
+</div>
+<!-- Dashboard -->
 <!-- javascript -->
 	<?php include 'view/script.php'; ?>
 <!-- javascript -->
