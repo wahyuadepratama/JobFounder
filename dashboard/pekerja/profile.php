48b89f6f0c4b42691a184748f45997ab694b9555
wahyuadepratama <wahyuadepratam4@gmail.com>
2017-07-07 09:15:29 +0700

    pindahkah profile, alur lebih sederhana
    profile dipindahkan ke dashboard (nama jadi pekerja), terus kemarin ada
    2 jenis pekerj (freelance sama kerja tetap), skrg wahyu jadikan 1 aja.
    ada di dalm folder dashboard

--- dashboard/pekerja/profile.php
+++ dashboard/pekerja/profile.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
 
-	<title>JobUs | Profile</title>
-	<?php include 'view/source2.php'; ?>
+	<title> JobUs | Profile Perusahaan </title>
+	<?php include '../../view/source.php'; ?>
 
 </head>
 	
@@ -12,28 +12,33 @@
 	<div class="banner1">
 		<div class="container">
 			
-		<?php include 'view/header2.php'; ?>
+			<?php include '../../view/header.php'; ?>
 
 		</div>
 	</div>
 <!-- banner -->
 
-<!-- breadcrumbs -->
-	<div class="breadcrumbs">
-		<div class="container">
-			<div class="w3layouts_breadcrumbs_left">
-				<ul>
-					<li><i class="glyphicon glyphicon-home" aria-hidden="true"></i><a href="home.php">Home</a><span>/</span></li>
-					<li><i class="glyphicon glyphicon-user" aria-hidden="true"></i>Profile</li>
-				</ul>
-			</div>
-			<div class="clearfix"> </div>
-		</div>
-	</div>
-<!-- //breadcrumbs -->
+<!-- profile -->
 <br>
 <div class="container">
+	<ul class="nav nav-pills">
+		<li role="presentation"><a href="track.php">Tracking</a></li>
+	  	<li role="presentation" class="active"><a href="profile.php">Profile</a></li>
+	</ul>
+	<ul class="">
+		
+	</ul>
+</div>
 
+<div class="container">
+	<center>
+		<img src="../../assets/images/invest.png" class="img-circle img-responsive" width="200"><br>
+		<div class="div-upload btn-default">
+		<span class="upload">Choose your photo profile</span>
+		<input type="file" class="file-upload">
+		</div>
+	</center>
+	<br>
 	<div class="form-group">
 		  <label for="company">Nama</label>
 		  <input type="text" class="form-control" name="nama" placeholder="ex: Yolanda Parawita">
@@ -43,7 +48,7 @@
 		  <input type="text" class="form-control" name="email" placeholder="ex: yolandaparawita@gmail.com">
 	</div>
 	<div class="form-group">
-		  <label for="fax">No Handphone</label>
+		  <label for="fax">Telepon/Fax</label>
 		  <input type="text" class="form-control" name="fax" placeholder="ex: 0813827381981 / 0751321452" >
 	</div>
 	<div class="form-group">
@@ -76,11 +81,15 @@
 		  <textarea class="form-control" rows="5" id="req" name="judul" placeholder="ex: Jl Khatib Sulaiman no 32"></textarea>
 	</div>
 	<div class="form-group">
+		  <label for="posisi">Keahlian</label>
+		  <textarea class="form-control" rows="5" id="req" name="judul" placeholder="ex: Programing, Desain"></textarea>
+	</div>
+	<div class="form-group">
 		    <label for="exampleInputFile">Upload Foto KTP</label><br><br>
 		    <input type="file" id="ktp">
 		    <p class="help-block">nb : ekstensi yang diterima hanya berupa jpg/ jpeg/ png</p>
-		    <p class="help-block">no : upload lah foto yang jelas dan bisa dibaca no ktp nya </p>
 	</div>
+
 	<div class="form-group">
 		<center>
 			<button type="submit" class="btn btn-default">Edit</button>
@@ -90,13 +99,8 @@
 </div>
 <!-- end profile -->
 
-<!-- footer -->
-	<?php include 'view/footer.php'; ?>
-<!-- //footer -->
-
 <!-- javascript -->
-	<?php include 'view/script2.php'; ?>
-
+	<?php include '../../view/script.php'; ?>
 <!-- javascript -->
 
 </body>
