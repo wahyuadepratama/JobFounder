53f7330a2782e306cae2380edabef74251ffeb48
rasakmarsawa <rasakmarsawa@gmail.com>
2017-07-13 16:57:34 +0700

    cara redirect http gmana yak?

--- daftar.php
+++ daftar.php
@@ -98,19 +98,26 @@
 		<div class="w3layouts-main" > 
 			<div class="agilesign-form">  
 				<div class="agileits-top">
-					<form action="dashboard/pekerja/profile.php" method="post">
+					<form action="" method="post">
+						<div class="form-group">
+							  <select class="form-control" id="sel1" name="sel1">
+							    <option value="0">Siapakah Anda ?</option>
+							    <option value="1">Individu / Pemilik Usaha Yang Butuh Pekerja</option>
+							    <option value="2">Pencari Lowongan Kerja</option>
+							  </select>
+						</div>
 						<div class="styled-input w3ls-text">
-							<input type="text" name="User_Name" required=""/>
+							<input type="text" name="lusername" required=""/>
 							<label>Username / Email</label>
 							<span></span>
 						</div>
 						<div class="styled-input w3ls-text">
-							<input type="password" name="Password" required=""> 
+							<input type="password" name="lpassword" required=""> 
 							<label>Password</label>
 							<span></span>
 						</div> 
 						<div class="agileits-bottom"> 
-							<input type="submit" value="Login"> 
+							<input type="submit" value="Login" name="login"> 
 						</div>	
 					</form>
 				</div> 
@@ -122,25 +129,71 @@
 	<!-- javascript -->
 		<?php 
 		include 'view/script2.php'; 
-		include 'kelas.php';
 
-		if(isset($_POST['sign'])){
-			if($_POST['sel2']==1){
-				$GLOBALS['objek'] = new pengiklan();
-			}elseif ($_POST['sel2']==2) {
-				$GLOBALS['objek'] = new pekerja();
-			}
-
-			$GLOBALS['objek']->username = $_POST['username'];
-			$GLOBALS['objek']->email = $_POST['email'];
-			$GLOBALS['objek']->no_hp = $_POST['no_handphone'];
-			$GLOBALS['objek']->password = $_POST['password'];
-
-			$GLOBALS['objek']->insert();
-
-		}
 
 		?>
 	<!-- javascript -->
 </body>
 </html>
+
+<?php 
+// SIGNUP
+	include 'kelas.php';
+	if(isset($_POST['sign'])){
+		if($_POST['sel2']==1){
+			$GLOBALS['objek'] = new pengiklan();
+		}elseif ($_POST['sel2']==2) {
+			$GLOBALS['objek'] = new pekerja();
+		}
+
+		$GLOBALS['objek']->username = $_POST['username'];
+		$GLOBALS['objek']->email = $_POST['email'];
+		$GLOBALS['objek']->no_hp = $_POST['no_handphone'];
+		$GLOBALS['objek']->password = md5($_POST['password']);
+
+		$GLOBALS['objek']->insert();
+	}
+
+
+// LOGIN
+	if(isset($_POST['login'])){
+		if($_POST['sel1']==1){
+			$GLOBALS['objek'] = new pengiklan();
+		}elseif ($_POST['sel2']==2) {
+			$GLOBALS['objek'] = new pekerja();
+		}
+
+		$data = $GLOBALS['objek']->select();
+
+		$n = 0;
+		while($n < count($data)){
+			if($data[$n]['username']==$_POST['lusername'] || $data[$n]['email']==$_POST['lusername']){
+
+
+				if($data[$n]['password']==md5($_POST['lpassword'])){
+
+					
+
+					if($_POST['sel1']==1){
+						$_SESSION['id']=$data[$n]['id_pengiklan'];
+						header('Location : dashboard/pemiliklowongan/profile');
+					}
+					elseif($_POST['sel1']==2){
+						$_SESSION['id']=$data[$n]['id_pekerja'];			
+						header('Location : dashboard/pekerja/profile');			
+					}
+
+
+					$_SESSION['username']=$data[$n]['username'];	
+
+					// header location error
+
+
+				}
+			}
+			$n++;			
+		}
+	}
+
+ ?>
+
