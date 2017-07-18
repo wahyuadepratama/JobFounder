6cd7905f60a0ffd7437b7f30948669f867ecae76
nabang97 <parawitayolanda@gmail.com>
2017-07-18 18:44:38 +0700

    Footer lua
    nanti dipoles lagi yaa.. muehehhe
    silakan yang mau ngoding selanjutnya

--- view/footer.php
+++ view/footer.php
@@ -2,17 +2,17 @@
 	<div class="footer">
 		<div class="container">
 			<div class="w3agile_footer_grids">
-				<div class="col-md-3 agileinfo_footer_grid">
-					<div class="w3_agile_logo">
-						<img src="assets/images/logofix.png" width="180"><br>
-					</div>
-				</div>
-				
-				<div class="col-md-2 agileinfo_footer_grid agileinfo_footer_grid1">
+			<div class="container">
+				<div class="col-md-6 ">
+					
+						<img src="assets/images/logofixx.png" class="img-responsive center-block">
 					
 				</div>
-				<div class="col-md-3 agileinfo_footer_grid">
-					<h3>Navigasi</h3>
+				
+				<div class="col-md-6 gileinfo_footer_grid">
+				
+					<h3 class="katangah">Navigasi</h3>
+					<div class="katangah">
 					<ul class="w3layouts_footer_nav">
 						<li><a href="home.php"><i class="glyphicon glyphicon-triangle-right" aria-hidden="true"></i>Home</a></li>
 						<li><a href="lowongan.php"><i class="glyphicon glyphicon-triangle-right" aria-hidden="true"></i>Lowongan</a></li> 
@@ -20,9 +20,10 @@
 						<li><a href="about.php"><i class="glyphicon glyphicon-triangle-right" aria-hidden="true"></i>Tentang Kami</a></li>
 						<li><a href="question.php"><i class="glyphicon glyphicon-triangle-right" aria-hidden="true"></i>FAQ</a></li> 					
 					</ul>
-					<div class="clearfix"> </div>
+					</div>
+					
 				</div>
-				<div class="clearfix"> </div>
+				</div>
 			</div>
 		</div>
 		<div class="w3_agileits_footer_copy">
